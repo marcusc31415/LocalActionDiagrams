3fb43038dc162d7c694a660abd76a2c2c38fd43f
marcusc31415 <darthmonks@gmail.com>
2025-09-15 02:22:07 +1000

    Documentation

--- gap/LocalActionDiagrams.gd
+++ gap/LocalActionDiagrams.gd
@@ -2,10 +2,31 @@
 # Local Action Diagrams #
 #########################
 
-#! @Chapter Introduction
-#! @Section Constructing Local Action Diagrams
+#! @Chapter Introduction 
+#!
+#! A local action diagram <M>\Delta = (\Gamma, (G(v)), (X_a))</M> consists of: 
+#! - a directed graph <M>\Gamma</M>, 
+#! - a closed permutation group <M>G(v)</M> for each <M>v \in V(\Gamma)</M>, and 
+#! - a set <M>X_{a}</M> for each <M>a \in A(\Gamma)</M> such that each <M>X_{a}</M> is disjoint and <M>X_{a}</M> is an orbit of the action of <M>G(o(a))</M> on <M>X_{v} := \bigsqcup_{a \in o^{-1}(v)}X_{a}</M>. 
+#!
+#! The definition of digraph used is different to that of the <Package>Digraphs</Package> package. For our purposes, a digraph <M>\Gamma</M> consists of:
+#! - a vertex set <M>V</M>,
+#! - an arc set <M>A</M>,
+#! - a map <M>o : A \to V</M> assigning each arc to an <E>origin</E> vertex, and
+#! - a bijection <M>r : A \to A</M> (denoted by <M>a \mapsto \overline{a}</M>) such that <M>r^{2} =  \mathrm{id}</M>. 
+#! The bijection <M>r</M> defines a reverse arc for each arc of the graph. This is more specific than the definition in the <A>digraphs</A> package which does not require a reverse mapping. 
+#!
+#! The local action diagrams package provides a category for local action diagrams. It is built in the <A>IsDigraph</A> category from the <Package>Digraphs</Package> package. 
+#!
 
-# Category For Local Action Diagrams
+#! @Chapter Creating Local Action Diagrams
+#! @Section Creating Local Action Diagrams 
+
+## Category For Local Action Diagrams ##
+#! @Returns <K>true</K> if <A>lad</A> is of the category <K>IsLocalActionDiagram</K> and <K>false</K> otherwise. 
+#! @Arguments lad
+#! @Description
+#! Every local action diagram belongs to the <C>IsLocalActionDiagram</C> category. Every local action diagram is immutable. 
 DeclareCategory("IsLocalActionDiagram", IsDigraph);
 
 # Local Action Diagram Attributes
@@ -17,11 +38,12 @@ DeclareAttribute("LocalActionDiagramScopos", IsLocalActionDiagram);
 DeclareAttribute("LocalActionDiagramGroupType", IsLocalActionDiagram);
 DeclareAttribute("LocalActionDiagramIsDiscrete", IsLocalActionDiagram);
 
-#! @BeginGroup Construction
+#! @BeginGroup Creating Local Action Diagrams
 #! @GroupTitle Constructing From Data
 
 #! @Arguments D, v_labels, e_labels, rev
 #! @Returns A local action diagram.
+#! @Description
 #! @Description
 #! 	Constructs a local action diagram, checking that the arguments given are a valid local action diagram. The argument <A>D</A> is a digraph and <A>rev</A> must be a compatible involution on the edges of <A>D</A>. The argument <A>v_labels</A> is a list of vertex labels such that <C><A>v_labels</A>[i]</C> is the group labelling vertex <C>i</C> of <A>D</A>. 
 #!
--- makedoc.g
+++ makedoc.g
@@ -7,4 +7,13 @@ if fail = LoadPackage("AutoDoc", "2018.02.14") then
     Error("AutoDoc version 2018.02.14 or newer is required.");
 fi;
 
-AutoDoc( rec( scaffold := true, autodoc := true ) );
+AutoDoc( rec( 
+    autodoc := true,
+    gapdoc := rec(
+        LaTeXOptions := rec( EarlyExtraPreamble := """
+            \usepackage{a4wide}
+            \newcommand{\bbZ}{\mathbb{Z}}
+        """ )
+    ),
+    scaffold := true 
+));
